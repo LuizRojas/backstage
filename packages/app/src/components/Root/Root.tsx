fa78ee3bde7b8e3216811fed183bf35a3ffe12d3
LuizRojas <luizfrojas@gmail.com>
2025-09-24 15:25:44 -0300

    fix: removendo imports não usados

--- packages/app/src/components/Root/Root.tsx
+++ packages/app/src/components/Root/Root.tsx
@@ -1,6 +1,5 @@
 import { PropsWithChildren } from 'react';
 import { makeStyles } from '@material-ui/core';
-import HomeIcon from '@material-ui/icons/Home';
 import ExtensionIcon from '@material-ui/icons/Extension';
 import LibraryBooks from '@material-ui/icons/LibraryBooks';
 import CreateComponentIcon from '@material-ui/icons/AddCircleOutline';
